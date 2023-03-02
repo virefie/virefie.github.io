e42c42485847e5047a82043b8479bb807209b316
virefie <saiger777@mailsaviors.com>
2023-03-02 15:42:41 +0100

    up

--- login/add/allPage/main.js
+++ login/add/allPage/main.js
@@ -1,6 +1,6 @@
-import { database, ref, child, get, gebi } from "../../../main.js";
+/* import { database, ref, child, get, gebi } from "../../../main.js";
 import { remove} from "https://www.gstatic.com/firebasejs/9.17.1/firebase-database.js";
-
+ */
 const dbRef = ref(database);
 const edtSVG= '<svg xmlns="http://www.w3.org/2000/svg" height="24" width="24"><path d="m19.3 8.925-4.25-4.2 1.4-1.4q.575-.575 1.413-.575.837 0 1.412.575l1.4 1.4q.575.575.6 1.388.025.812-.55 1.387ZM17.85 10.4 7.25 21H3v-4.25l10.6-10.6Z"/></svg>'
 
