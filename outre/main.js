9af53778b0d56c8a1015efe097a8fd743798c523
virefie <saiger777@mailsaviors.com>
2023-03-04 19:53:03 +0100

    up

--- outre/main.js
+++ outre/main.js
@@ -177,36 +177,56 @@ window.hiding = hiding;
 
 /* instalation app */
 let deferredPrompt;
+/* if app is instal */
+window.addEventListener('appinstalled', () => {
+    // Hide the app-provided install promotion
+    gebi('dwnld').className ='n';
+    // Clear the deferredPrompt so it can be garbage collected
+    deferredPrompt = null;
+    /* // Optionally, send analytics event to indicate successful install
+    console.log('PWA was installed'); */
+  });
 
 window.addEventListener('beforeinstallprompt', (e) => {
-  // Prevent the mini-infobar from appearing on mobile
   e.preventDefault();
-  // Stash the event so it can be triggered later.
   deferredPrompt = e;
   // Update UI notify the user they can install the PWA
-  //showInstallPromotion();
+  let os= navigator.userAgent.toLocaleLowerCase();
+
+if (os.includes('android') && os.includes('chrome')) {
+    gebi('dwnld').className = 'dwnld';
+}
   // Optionally, send analytics event that PWA install promo was shown.
   console.log(`'beforeinstallprompt' event was fired.`);
 });
 let buttonInstall=gebi('dwnld');
 buttonInstall.addEventListener('click', async () => {
-    // Hide the app provided install promotion
     gebi('dwnld').className ='n';
-    // Show the install prompt
     deferredPrompt.prompt();
-    // Wait for the user to respond to the prompt
+
     const { outcome } = await deferredPrompt.userChoice;
-    // Optionally, send analytics event with outcome of user choice
+
     console.log(`User response to the install prompt: ${outcome}`);
-    // We've used the prompt, and can't use it again, throw it away
     deferredPrompt = null;
   });
-/* if app is instal */
-  window.addEventListener('appinstalled', () => {
-    // Hide the app-provided install promotion
-    gebi('dwnld').className ='n';
-    // Clear the deferredPrompt so it can be garbage collected
-    deferredPrompt = null;
-    // Optionally, send analytics event to indicate successful install
-    console.log('PWA was installed');
-  });
+
+
+/* 
+  function getPWADisplayMode() {
+    const isStandalone = window.matchMedia('(display-mode: standalone)').matches;
+    if (document.referrer.startsWith('android-app://')) {
+      return 'twa';
+    } else if (navigator.standalone || isStandalone) {
+      return 'standalone';
+    }
+    return 'browser';
+  } */
+/* 
+  window.matchMedia('(display-mode: standalone)').addEventListener('change', (evt) => {
+    let displayMode = 'browser';
+    if (evt.matches) {
+      displayMode = 'standalone';
+    }
+    // Log display mode change to analytics
+    console.log('DISPLAY_MODE_CHANGED', displayMode);
+  }); */
