7230394e6fab2098d68daf53a31f73e6f0b1ad43
virefie <saiger777@mailsaviors.com>
2023-03-08 17:58:57 +0100

    up

--- outre/js/hdr.js
+++ outre/js/hdr.js
@@ -8,21 +8,21 @@ let orgn = location.origin, path = window.location.pathname,
                     <span class="icon"><span></span><span></span><span></span>
                     </span>
                     <ul>
-                        <li id= "/">
-                            <a href="${orgn}" class="aboutUs"> الصفحة الرئيسية</a>
+                        <li>
+                            <a id= "/" href="${orgn}" class="aboutUs"> الصفحة الرئيسية</a>
                         </li>
-                        <li id = "/call">
-                            <a href="${orgn}/call/index.min.html">اتصل بنا</a>
+                        <li>
+                            <a id = "/call" href="${orgn}/call/index.min.html">اتصل بنا</a>
                         </li>
-                        <li id = "/aboutus">
-                            <a href="${orgn}/aboutus/index.min.html"> من نحن</a>
+                        <li >
+                            <a id = "/aboutus" href="${orgn}/aboutus/index.min.html"> من نحن</a>
                         </li>
-                        <li id="/cnfdnt">
-                            <a href="${orgn}/cnfdnt/index.min.html"> سياسة الخصوصية</a>
+                        <li >
+                            <a id="/cnfdnt" href="${orgn}/cnfdnt/index.min.html"> سياسة الخصوصية</a>
                         </li>
                     </ul>
                 </div>
             </div>`;
 if(path.length > 1){path =path.slice(0,path.indexOf('/index'))};
 
-document.getElementById(path).classList= 'active';
+document.getElementById(path).classList += ' active';
