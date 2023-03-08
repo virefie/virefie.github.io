831f02ec2bd99351a406ef3de137a4b42d07eb6a
virefie <saiger777@mailsaviors.com>
2023-03-08 22:28:15 +0100

    updt

--- outre/js/main.js
+++ outre/js/main.js
@@ -1,15 +1,16 @@
-import { database, ref, child, get, gebi} from "../../main.js";
-class base { valed = []; Fake = [];
-    getInd (e) {
+import { database, ref, child, get, gebi } from "../../main.js";
+class base {
+    valed = []; Fake = [];
+    getInd(e) {
         let ind = this.valed.indexOf(e);
         if (ind > -1) {
-            return {vl: "valed",in: ind}
-        } 
+            return { vl: "valed", in: ind }
+        }
         ind = this.Fake.indexOf(e)
-        if (ind > -1 ) {
-            return {vl: "Fake",in: ind}
-        } 
-        return {vl: "Uncertain",in: ind}
+        if (ind > -1) {
+            return { vl: "Fake", in: ind }
+        }
+        return { vl: "Uncertain", in: ind }
         /* return ind > -1 ? {
             
         } : (ind = this.Fake.indexOf(e)? {
@@ -18,10 +19,10 @@ class base { valed = []; Fake = [];
         } : (ind = 0, {
             vl: "Uncertain",
             in: ind
-        })) */x 
+        })) */x
     };
 };
-let aSn = { valed: [], Fake: []}, vUp = { lvrFake: 0,lvrvaled: 0 ,dwAapp:0},frm = document.forms[0], 
+let aSn = { valed: [], Fake: [] }, vUp = { lvrFake: 0, lvrvaled: 0, dwAapp: 0 }, frm = document.forms[0],
     dv = gebi("divplac"), emails = new base, Ccps = new base, fcbs = new base;
 
 const dbRef = ref(database);
@@ -33,7 +34,7 @@ function rslt(e) {
     get(child(dbRef, `${e}/`)).then(t => {
         aSn[e] = Object.entries(t.val());
         localStorage.setItem("lst" + e, JSON.stringify(aSn[e]));
-         stlstPg(e)
+        stlstPg(e)
     })
 }
 
@@ -47,7 +48,7 @@ if (os.includes('android') && os.includes('chrome')) {
   */
 
 function stlstPg(e) {
-    let cmprNmbr = (a, b)=> a[0].slice(1) - b[0].slice(1);//,t = "";
+    let cmprNmbr = (a, b) => a[0].slice(1) - b[0].slice(1);//,t = "";
     aSn[e].sort(cmprNmbr);
     aSn[e].sort(cmprNmbr).forEach(a => {
         emails[e].push(a[1][3]), Ccps[e].push(a[1][2]), fcbs[e].push(a[1][1]);
@@ -56,32 +57,33 @@ function stlstPg(e) {
             s = a[1][2].length > 0 ? "0079999900" + a[1][2] : "";
         t += `<tr><td>${s}</td><td>${a[1][3]}</td><td><a ${l}>${a[1][0]}</a><p>${a[1][4]}</p></td></tr>` */
     });// gebi("bdy" + e).innerHTML = t;
-    if ("Fake" == e ) {
+    if ("Fake" == e) {
         gebi("lod").className = "n";
-        gebi("plWt").className = "n"   
+        gebi("plWt").className = "n";
+        document.querySelector('header').style.display = 'block';
     }
-   
+
 }
 
 function afchRslt(e) {
     //console.log(['Array'].forEach);console.log(fcbs);
-    gebi("cntnr").style.display = "flex"; 
+    gebi("cntnr").style.display = "flex";
     setTimeout(() => {
         dv.className += " opPlc"
-    }, 0);  
-    if ( e.vl == "Uncertain") {
+    }, 0);
+    if (e.vl == "Uncertain") {
         dv.innerHTML = '<span class="clear" onclick="hiding()">×</span>\n       <div class="Uncertain hidplac">\n      <h2>لم يتم التأكد من مصداقية هد الحساب</h2>\n      <span> ليس لدينا معلوما حول هذا الحساب يرجى مساعدتنا عبر صفحتنا على الفيسبوك </span><a href="https://www.facebook.com/profile.php?id=100089115259293" >  مكافحة النصب الإلكتروني</a></div> ';
-       return 0
+        return 0
     }
-     
+
     let t = aSn[e.vl][e.in][1],
-        a = "valed" == e.vl ? "هذا الحساب موثوق" : 'حذاري التعامل مع هذا الشخص، نصاب ⚠️' ,
-        l = t[1].length > 0 ? `<h2> : إسم الحساب<br><a href="https://www.facebook.com/'${t[1]}" target="_blank"> ${t[0]}</a></h2>  `: "",
+        a = "valed" == e.vl ? "هذا الحساب موثوق" : 'حذاري التعامل مع هذا الشخص، نصاب ⚠️',
+        l = t[1].length > 0 ? `<h2> : إسم الحساب<br><a href="https://www.facebook.com/'${t[1]}" target="_blank"> ${t[0]}</a></h2>  ` : "",
         s = t[2].length > 0 ? `<strong> :رقم الحساب البريدي </strong><br>0079999900${t[2]}<br><br>` : "",
         n = t[3].length > 0 ? `<strong>:الايميل</strong> <br> ${t[3]}<br><br>` : "",
         r = t[4].length > 0 ? `<strong> :معلومات عنه </strong><br> ${t[4]}` : "";
     dv.innerHTML = `<span class="clear" onclick="hiding()">×</span>\n  <div class="dv${e.vl} hidjs"><h2>${a}</h2>${l} ${n + s + r}   </div>`;
-   
+
 }
 
 function hiding() {
@@ -90,7 +92,7 @@ function hiding() {
     }, 320)
 }
 
-window.onload = ()=>{
+window.onload = () => {
     if (window.navigator.onLine) {
         lodngVrsn()
     } else {
@@ -98,8 +100,8 @@ window.onload = ()=>{
             aSn.valed = JSON.parse(localStorage.lstvaled);
             stlstPg("valed");
             aSn.Fake = JSON.parse(localStorage.lstFake);
-            stlstPg("Fake") 
-        }else{
+            stlstPg("Fake")
+        } else {
             gebi('ntintrnt').innerText = 'ليس لديك اتصال بالانترنت'
         }
     }
@@ -107,16 +109,16 @@ window.onload = ()=>{
 
 function lodngVrsn() {
     get(child(dbRef, "updatV/"))
-    .then(e => {
-        vUp.dwAapp = e.val().dwAapp;
-        if ( localStorage.vrsnUp != null) {
-            vUp = JSON.parse(localStorage.vrsnUp);
-            
-                if (vUp.lvrvaled == e.val().vrvaled ) {
+        .then(e => {
+            vUp.dwAapp = e.val().dwAapp;
+            if (localStorage.vrsnUp != null) {
+                vUp = JSON.parse(localStorage.vrsnUp);
+
+                if (vUp.lvrvaled == e.val().vrvaled) {
                     aSn.valed = JSON.parse(localStorage.lstvaled);
                     stlstPg("valed")
-                }else{
-                    vUp.lvrvaled = e.val().vrvaled; 
+                } else {
+                    vUp.lvrvaled = e.val().vrvaled;
                     localStorage.setItem("vrsnUp", JSON.stringify(vUp));
                     rslt("valed")
                 }
@@ -128,52 +130,52 @@ function lodngVrsn() {
                     localStorage.setItem("vrsnUp", JSON.stringify(vUp));
                     rslt("Fake");
                 }
-            
-            /* }); */
-        }else{
-        /*  get(child(dbRef, "updatV/")).then(e => { */
-                vUp.lvrFake = e.val().vrFake; 
+
+                /* }); */
+            } else {
+                /*  get(child(dbRef, "updatV/")).then(e => { */
+                vUp.lvrFake = e.val().vrFake;
                 vUp.lvrvaled = e.val().vrvaled;
-                localStorage.setItem("vrsnUp", JSON.stringify(vUp)); 
+                localStorage.setItem("vrsnUp", JSON.stringify(vUp));
                 rslt("valed"); rslt("Fake")
-        
-        }
-    })
+
+            }
+        })
 }
 
- frm.onsubmit = (e => {
+frm.onsubmit = (e => {
     function t() {
         gebi("errCcp").style.display = "block", setTimeout(() => {
             gebi("errCcp").style.display = "none"
         }, 2e3)
     }
-    e.preventDefault();frm.srch.blur();
-     frm.srch.value = frm.srch.value.trim();
-    let vl = frm.srch.value,tpInp=gebi("tpInp").value, l = vl.indexOf("facebook.com"),
+    e.preventDefault(); frm.srch.blur();
+    frm.srch.value = frm.srch.value.trim();
+    let vl = frm.srch.value, tpInp = gebi("tpInp").value, l = vl.indexOf("facebook.com"),
         s = vl.length;
- 
-    if (tpInp=='rib'&& 0 == vl.indexOf("0079999900") && !isNaN(vl) && 20 == s ) {
-        vl = vl.slice(10, 20);  afchRslt(Ccps.getInd(vl));
+
+    if (tpInp == 'rib' && 0 == vl.indexOf("0079999900") && !isNaN(vl) && 20 == s) {
+        vl = vl.slice(10, 20); afchRslt(Ccps.getInd(vl));
         return 0
     }
-    if (tpInp=='Nccp' && !isNaN(vl) && vl.length == 12 && vl.indexOf("00")== 0 ) {
-        vl = vl.slice(2, 12) ;
+    if (tpInp == 'Nccp' && !isNaN(vl) && vl.length == 12 && vl.indexOf("00") == 0) {
+        vl = vl.slice(2, 12);
     }
-    if (tpInp=='Nccp' && vl.length == 10) {
+    if (tpInp == 'Nccp' && vl.length == 10) {
         afchRslt(Ccps.getInd(vl));
-        return 0 
+        return 0
     }
 
-    if (tpInp=='fcbURL' && l > -1) {
+    if (tpInp == 'fcbURL' && l > -1) {
         afchRslt(fcbs.getInd(vl.slice(l + "facebook.com/".length, s)));
         return 0
     }
-    if (tpInp=='eml' && vl.indexOf('@')>0) {
+    if (tpInp == 'eml' && vl.indexOf('@') > 0) {
         afchRslt(emails.getInd(vl));
         return 0
     }
     t();
-    
+
 });
 window.hiding = hiding;
 
@@ -186,35 +188,35 @@ let deferredPrompt;
     gebi('dwnld').className ='n';
     // Clear the deferredPrompt so it can be garbage collected
     deferredPrompt = null; */
-    /* // Optionally, send analytics event to indicate successful install
-    console.log('PWA was installed'); */
- /*  }); */
+/* // Optionally, send analytics event to indicate successful install
+console.log('PWA was installed'); */
+/*  }); */
 
 window.addEventListener('beforeinstallprompt', (e) => {
-  e.preventDefault();
-  deferredPrompt = e;
-  // Update UI notify the user they can install the PWA
-  let os= navigator.userAgent.toLocaleLowerCase();
+    e.preventDefault();
+    deferredPrompt = e;
+    // Update UI notify the user they can install the PWA
+    let os = navigator.userAgent.toLocaleLowerCase();
 
-if (os.includes('android') || os.includes('ipad') || os.includes('iphone')) {
-    gebi('dvdw').className = 'blck';
-}
+    if (os.includes('android') || os.includes('ipad') || os.includes('iphone')) {
+        gebi('dvdw').className = 'blck';
+    }
 
 });
 
-let buttonInstall=gebi('dwnld');
+let buttonInstall = gebi('dwnld');
 buttonInstall.addEventListener('click', async () => {
-    gebi('dvdw').className ='n';
+    gebi('dvdw').className = 'n';
     deferredPrompt.prompt();
     /* if (vUp.dwAapp >0) {
         
     } */
-/* 
-    const { outcome } = await deferredPrompt.userChoice;
-
-    console.log(`User response to the install prompt: ${outcome}`); */
+    /* 
+        const { outcome } = await deferredPrompt.userChoice;
+    
+        console.log(`User response to the install prompt: ${outcome}`); */
     deferredPrompt = null;
-  });
+});
 
 
 /* 
